181cbd3a372a4539b20b8213e64332fe1bf56811
Mark Ian Ebuen <ianebuen2001@gmail.com>
2025-11-06 03:40:11 +0800

    Initial commit

---
+++ src/main/java/com/devian/larapwebsite/LarapWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.devian.larapwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LarapWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LarapWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devian/larapwebsite/LarapWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devian.larapwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LarapWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
